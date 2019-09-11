b2fd23d4ee9c01738329ef9f7b941e605372c479
amserhii11 <amelin.serhii@gmail.com>
2019-09-12 00:17:25 +0300

    Updates

---
+++ precache-manifest.f9280cd08cfa3626ce93e6034e8f7216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ef7f614db88ff9d1b90d3686181164",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "dc411f4166812541ccfd",
+    "url": "/react_static-list-of-posts/static/css/main.bb523d0d.chunk.css"
+  },
+  {
+    "revision": "58ca6ceb802300dae504",
+    "url": "/react_static-list-of-posts/static/js/2.d97d6a68.chunk.js"
+  },
+  {
+    "revision": "dc411f4166812541ccfd",
+    "url": "/react_static-list-of-posts/static/js/main.65ca5cdb.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
